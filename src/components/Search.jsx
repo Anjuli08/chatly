3daf120fa230e2030db46983389b8973b88b6e7e
Anjuli08 <anjuliaggarwal8@gmail.com>
2023-05-07 10:45:32 +0530

    works

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className='searchForm'>
+        <input type='text' placeholder='find a user'/>
+      </div>
+      <div className='userChat'>
+        <img src='https://images.pexels.com/photos/4919373/pexels-photo-4919373.jpeg' alt='' />
+
+        <div className='userChatInfo'>
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
