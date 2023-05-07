57afac7eb784cc90798b532a0f64798fe31d208e
Anjuli08 <anjuliaggarwal8@gmail.com>
2023-05-07 21:00:53 +0530

    works

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Add from '../img/addimg.png'
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage } from "../firebase";
