1f585ded128642d876bf5831927ada91d67d8402
Anjuli08 <anjuliaggarwal8@gmail.com>
2023-05-12 21:03:13 +0530

    send realtime chat message

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Add from '../img/add.png';
 import More from '../img/more.png';
 import Cam from '../img/cam.png';
 import Messages from './Messages.jsx';
 import Input from './Input.jsx';
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
+  const { data } = useContext(ChatContext);
+
   return (
     <div className='chat'>
       <div className='chatInfo'>
-        <span>Jane</span>
+        <span>{data.user?.displayName}</span>
         <div className='chatIcons'>
           <img src={Cam} alt='' />
           <img src={Add} alt='' />
@@ -20,7 +23,7 @@ const Chat = () => {
       <Messages />
       <Input />
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
